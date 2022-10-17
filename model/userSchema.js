e018bb46fca0d5c6754c635e7fcc58f27b225694
joshna komati <komatijoshna123@gmail.com>
2022-10-17 09:50:24 +0530

    secound

--- model/userSchema.js
+++ model/userSchema.js
@@ -3,7 +3,7 @@ const usermodel=new mongoose.Schema({
     name:String,
     age:Number,
     rollno :Number,
-    classes:Number
+    classes:String
 })
 module.exports=new mongoose.model('student',usermodel)
 db.post.agg
